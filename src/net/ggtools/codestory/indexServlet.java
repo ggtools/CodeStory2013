4e53822143642201a12330b3bdef11dacced0109
Christophe Labouisse <christophe@labouisse.org>
2013-01-15 13:42:28 +0100

    Project creation.

---
+++ src/net/ggtools/codestory/indexServlet.java
@@ -0,0 +1,20 @@
+package net.ggtools.codestory;
+
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cele
+ * Date: 15/01/13
+ * Time: 13:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class indexServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
