3e9863b4ef7cf10b062cbbb9e34f3215cafd73b2
Christophe Labouisse <christophe@labouisse.org>
2013-01-17 14:46:24 +0100

    Implement calculator resolver.

--- src/main/java/net/ggtools/codestory/CalculatorResolver.java
+++ src/main/java/net/ggtools/codestory/CalculatorResolver.java
@@ -1,6 +1,12 @@
 package net.ggtools.codestory;
 
+import de.congrace.exp4j.Calculable;
+import de.congrace.exp4j.ExpressionBuilder;
+
 import javax.servlet.http.HttpServletRequest;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 /**
  * User: Christophe Labouisse
@@ -8,8 +14,20 @@ import javax.servlet.http.HttpServletRequest;
  * Time: 11:43
  */
 public class CalculatorResolver implements Resolver {
+
     @Override
     public String solve(HttpServletRequest request) throws ResolverException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        String q = request.getParameter("q");
+
+        if (q == null) return null;
+
+        String expr = q.replace(',', '.').replaceAll(" ", "+");
+        try {
+            Calculable calculable = new ExpressionBuilder(expr).build();
+            DecimalFormat format = new DecimalFormat("0.##", new DecimalFormatSymbols(Locale.FRENCH));
+            return format.format(calculable.calculate());
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
