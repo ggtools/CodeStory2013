c0da36785ccb59c3df66fd4ecccae63833403391
Christophe Labouisse <cele@ghxfrance.com>
2013-01-18 14:44:34 +0100

    Preparing jajascript question.

--- src/main/java/net/ggtools/codestory/QuestionResolver.java
+++ src/main/java/net/ggtools/codestory/QuestionResolver.java
@@ -17,6 +17,8 @@ public class QuestionResolver implements Resolver {
 
     private enum QUESTION {
 
+        SECOND_MD("As tu bien recu le second enonce(OUI/NON)", "OUI"),
+        GOOD_NIGHT("As tu passe une bonne nuit malgre les bugs de l etape precedente(PAS_TOP/BOF/QUELS_BUGS)", "QUELS_BUGS"),
         FIRST_MD("As tu bien recu le premier enonce(OUI/NON)", "OUI"),
         ALWAYS_YES("Est ce que tu reponds toujours oui(OUI/NON)", "NON"),
         MD_POST("Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)", "OUI"),
