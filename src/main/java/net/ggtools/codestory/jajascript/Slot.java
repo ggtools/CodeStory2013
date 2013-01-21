b705731e2ca3bf88aac9023ed44d628f647ce794
Christophe Labouisse <cele@ghxfrance.com>
2013-01-21 15:09:07 +0100

    Fixed URL when the day was longer than 24 hours. Implemented new Question.

--- src/main/java/net/ggtools/codestory/jajascript/Slot.java
+++ src/main/java/net/ggtools/codestory/jajascript/Slot.java
@@ -1,7 +1,6 @@
 package net.ggtools.codestory.jajascript;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
@@ -12,15 +11,12 @@ import java.util.List;
  * Date: 21/01/13
  * Time: 07:05
  */
-@RequiredArgsConstructor
 @Getter
 @Setter
 class Slot {
-    private final int start;
-
-    private int gain;
-
-    private final List<Flight> path = new ArrayList<>(OptimizeResolver.MAX_SLOTS);
+    private final List<Flight> path = new ArrayList<>();
 
     private final List<Flight> flights = new ArrayList<>();
+
+    private int gain;
 }
